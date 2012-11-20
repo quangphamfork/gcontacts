1f2867dcab452d588083adacab269ce5e7d6691a
gagoar <xeroice@gmail.com>
2012-11-20 13:40:25 -0300

    ahora, triggereamos eventos, dependiendo de el request todo evento tiene un array de objetos asociado bajo event.data

--- src/gcontacts.js
+++ src/gcontacts.js
@@ -4,8 +4,12 @@ var Gcontacts = {
     origin: window.location.href.replace(window.location.pathname,''),
     redirect_uri: window.location.href,
     header: {token: 'Authorization'},
-    group:{ from: 'https://www.google.com/m8/feeds/groups/default/', alt: 'json', projection: 'thin' }
+    group:{ from: 'https://www.google.com/m8/feeds/groups/default/', alt: 'json', projection: 'thin' },
+    contacts: { by_group: 'https://www.google.com/m8/feeds/contacts/default/full/?', alt: 'json' }
   },
+  events: {
+  },
+  contacts: {},
   groups: {},
   token_data: {valid: false},
   parameters: {
@@ -34,14 +38,19 @@ var Gcontacts = {
         if(window._GcontactsTokenTimeout != 'undefined') clearTimeout(window._GcontactsTokenTimeout);
         for(attr in config) this.parameters[attr] = config[attr];
         this.check_config();
+        this.create_custom_events();
         window.addEventListener('message', Gcontacts.message,  false);
-      }else throw 'worng  initialization';
+      }else throw 'wrong initialization';
     },
     check_config: function(){
       for(var property in this.parameters){
         if(typeof(this.parameters[property]) !== 'string' || this.parameters[property] === '') throw [property,'Its different than we spect!'].join(' ');
       }
       return true;
+    },
+    create_custom_events: function(){
+      this.events.contacts = new CustomEvent('gc.contacts');
+      this.events.groups = new CustomEvent('gc.groups');
     },
     message: function(event){
       with(Gcontacts){
@@ -67,16 +76,15 @@ var Gcontacts = {
         }
       }
     },
+    get_xhr: function(){
+      if(Gcontacts.xhr == undefined) Gcontacts.xhr = new XMLHttpRequest();
+    },
     get_groups: function(callback){
        if(Gcontacts.token_data.valid){
          var group = Gcontacts.config.group;
          var url = group.from + group.projection + '?alt=' + group.alt;
          var header = [Gcontacts.token_data.token_type,Gcontacts.token_data.access_token].join(' ');
-         console.log(group);
-         console.log(url);
-         console.log(Gcontacts.config.header.token);
-         console.log(header);
-         Gcontacts.xhr = new XMLHttpRequest();
+         this.get_xhr();
          Gcontacts.xhr.open('GET', url, true);
          Gcontacts.xhr.setRequestHeader('Authorization', header);
          Gcontacts.xhr.withCredentials = true;
@@ -84,14 +92,46 @@ var Gcontacts = {
          Gcontacts.xhr.send();
        }
     },
-    conso: function(){
-      for(i in Gcontacts.groups.feed.entry) console.log(Gcontacts.groups.feed.entry[i].content.$t)
+    get_group: function(group_link,callback){
+       if(Gcontacts.token_data.valid){
+         var contacts = Gcontacts.config.contacts;
+         var url = [contacts.by_group, 'group=', group_link, '&alt=',contacts.alt].join('');
+         var header = [Gcontacts.token_data.token_type,Gcontacts.token_data.access_token].join(' ');
+         this.get_xhr();
+         Gcontacts.xhr.open('GET', url, true);
+         Gcontacts.xhr.setRequestHeader('Authorization', header);
+         Gcontacts.xhr.withCredentials = true;
+         Gcontacts.xhr.onload = callback;
+         Gcontacts.xhr.send();
+       }
     },
     get_groups_response: function(){
       Gcontacts.groups = JSON.parse(Gcontacts.xhr.response);
-      Gcontacts.conso();
+      var groups = [];
+      with(Gcontacts.groups.feed){
+        for(i in entry){
+          groups.push(Object.create({},{ name: {value: entry[i].title.$t}, id: {value: entry[i].id.$t}}));
+        }
+      }
+      Gcontacts.events.groups.data = groups;
+      document.dispatchEvent(Gcontacts.events.groups);
+    },
+    get_contacts_response: function(){
+      console.log('heui');
+      Gcontacts.contacts = JSON.parse(Gcontacts.xhr.response);
+      var contacts = [];
+      with(Gcontacts.contacts.feed){
+        for(i in entry){
+          contacts.push(Object.create({},{ name: {value: entry[i].title.$t}, email: {value: entry[i].gd$email}}));
+        }
+      }
+      Gcontacts.events.contacts.data = contacts;
+      document.dispatchEvent(Gcontacts.events.contacts);
     },
     show_groups: function(){
       if(Gcontacts.groups != 'undefined') Gcontacts.get_groups(Gcontacts.get_groups_response);
+    },
+    get_contacts_by_group: function(group){
+      if(Gcontacts.contacts.group != group) Gcontacts.get_group(group,Gcontacts.get_contacts_response);
     }
 }
