5f19c1eb50a1a064e52c21fa3ec6c5fd2050f121
gagoar <xeroice@gmail.com>
2012-11-21 18:16:45 -0300

    convertido el objeto en modulo para tener metodos publicos y privados y proteger la integridad de la herramienta

--- src/gcontacts.js
+++ src/gcontacts.js
@@ -1,73 +1,73 @@
-var Gcontacts = {
-  config: {
+var Gcontacts = (function(){
+  var xhr = new XMLHttpRequest();
+  config = {
     url: 'https://accounts.google.com/o/oauth2/auth',
     origin: window.location.href.replace(window.location.pathname,''),
     redirect_uri: window.location.href,
     header: {token: 'Authorization'},
     group:{ from: 'https://www.google.com/m8/feeds/groups/default/', alt: 'json', projection: 'thin' },
     contacts: { by_group: 'https://www.google.com/m8/feeds/contacts/default/thin/?', alt: 'json' }
-  },
-  events: {},
-  contacts: {},
-  groups: {},
-  token_data: {valid: false},
-  parameters: {
+  };
+  events = {};
+  contacts = {};
+  groups = {};
+  token_data = {valid: false};
+  parameters = {
     response_type: 'token',
     client_id: '896644733940.apps.googleusercontent.com',
     scope: 'https://www.google.com/m8/feeds',
     inmediate: 'true',
-  },
-  auth: function(href){
+  };
+  check_config = function(){
+    for(var property in parameters){
+      if(typeof(parameters[property]) !== 'string' || parameters[property] === '')
+        throw [property,'Its different than we spect!'].join(' ');
+    }
+    return true;
+  };
+  auth = function(href){
     var url = [];
-    for(var property in this.config) if (property !==  'url')
-      url.push([encodeURIComponent(property), encodeURIComponent(this.config[property])].join('='));
-    for(var property in this.parameters)
-      url.push([encodeURIComponent(property), encodeURIComponent(this.parameters[property])].join('='));
-    return [(href ? href : this.config.url), url.join('&')].join('?');
-  },
-  login: function(href){
-    if(this.check_config()){
-      url = Gcontacts.auth(href);
+    for(var property in config)
+      if(property !== 'url')
+        url.push([encodeURIComponent(property), encodeURIComponent(config[property])].join('='));
+    for(var property in parameters)
+      url.push([encodeURIComponent(property), encodeURIComponent(parameters[property])].join('='));
+    return [(href ? href : config.url), url.join('&')].join('?');
+  };
+  login = function(href){
+    if(check_config()){
+      url = auth(href);
       window.open(url, "_blank", ['toolbar=no', 'location= '+ (window.opera ? 'no' : 'yes'), 'directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,copyhistory=no', 'width=' + window.screen.width / 2, 'height='+ window.screen.height / 2 ].join())
-    }},
-    init: function(config){
-      if (window.location.hash !== '') {
-        window.opener.postMessage(window.location.hash, window.location.origin);
-        window.close();
-      }
-      if(typeof(config) !== 'undefined'){
-        if(window._GcontactsTokenTimeout != 'undefined') clearTimeout(window._GcontactsTokenTimeout);
-        for(attr in config) this.parameters[attr] = config[attr];
-        this.check_config();
-        this.create_custom_events();
-        window.addEventListener('message', Gcontacts.message,  false);
-      }else throw 'wrong initialization';
-    },
-    check_config: function(){
-      for(var property in this.parameters){
-        if(typeof(this.parameters[property]) !== 'string' || this.parameters[property] === '')
-          throw [property,'Its different than we spect!'].join(' ');
-      }
-      return true;
-    },
-    create_custom_events: function(){
-      this.events.contacts = new CustomEvent('gc.contacts');
-      this.events.contacts.error = new CustomEvent('gc.contacts.error');
-      this.events.groups = new CustomEvent('gc.groups');
-      this.events.groups.error = new CustomEvent('gc.groups.error');
-    },
-    message: function(event){
-      with(Gcontacts){
+    }
+  };
+  init = function(config){
+    if (window.location.hash !== '') {
+      window.opener.postMessage(window.location.hash, window.location.origin);
+      window.close();
+    }
+    if(typeof(config) != undefined){
+      if(window._GcontactsTokenTimeout != undefined)
+        clearTimeout(window._GcontactsTokenTimeout);
+      for(attr in config)
+        parameters[attr] = config[attr];
+      check_config();
+      create_custom_events();
+      window.addEventListener('message', message,  false);
+    }else throw 'wrong initialization';
+  };
+   create_custom_events =  function(){
+      events.contacts = new CustomEvent('gc.contacts');
+      events.groups = new CustomEvent('gc.groups');
+    };
+   message = function(event){
         var data = event.data.split('&');
         for (i in data){
           var content = data[i].match('(.*)=(.*)').splice(1,3);
           token_data[content[0].replace(/^#/,'')] = content[1];
         }
-        if (token_data.token != 'undefined' && token_data.expire_in != 'undefined') Gcontacts.set_token_state(true);
-      }
-    },
-    set_token_state: function(state){
-      with(Gcontacts){
+        if (token_data.token != 'undefined' && token_data.expire_in != 'undefined') set_token_state(true);
+    };
+    set_token_state = function(state){
         if (state){
           var msec = Number(token_data.expires_in) * Number(1000);
           window._GcontactsTokenTimeout = setTimeout(set_token_state, msec);
@@ -78,76 +78,76 @@ var Gcontacts = {
           token_data.valid = false;
           throw(['token expired on', Date()].join(' '));
         }
-      }
-    },
-    get_xhr: function(){
-      if(Gcontacts.xhr == undefined) Gcontacts.xhr = new XMLHttpRequest();
-    },
-    get_groups: function(callback){
-       if(Gcontacts.token_data.valid){
-         var group = Gcontacts.config.group;
-         var url = group.from + group.projection + '?alt=' + group.alt;
-         var header = [Gcontacts.token_data.token_type,Gcontacts.token_data.access_token].join(' ');
-         this.get_xhr();
-         Gcontacts.xhr.open('GET', url, true);
-         Gcontacts.xhr.setRequestHeader('Authorization', header);
-         Gcontacts.xhr.withCredentials = true;
-         Gcontacts.xhr.onload = (callback != undefined) ? callback : Gcontacts.get_groups_response;
-         Gcontacts.xhr.send();
+    };
+    get_groups = function(callback){
+       if(token_data.valid){
+         group = config.group;
+         url = group.from + group.projection + '?alt=' + group.alt;
+         header = [token_data.token_type,token_data.access_token].join(' ');
+         //get_xhr();
+         xhr.open('GET', url, true);
+         xhr.setRequestHeader('Authorization', header);
+         xhr.withCredentials = true;
+         xhr.onload = (callback != undefined) ? callback : get_groups_response;
+         xhr.send();
        }else throw 'invalid token';
-    },
-    get_group: function(group_link,callback){
-       if(Gcontacts.token_data.valid){
-         var contacts = Gcontacts.config.contacts;
-         var url = [contacts.by_group, 'group=', group_link, '&alt=',contacts.alt].join('');
-         var header = [Gcontacts.token_data.token_type,Gcontacts.token_data.access_token].join(' ');
-         this.get_xhr();
-         Gcontacts.xhr.open('GET', url, true);
-         Gcontacts.xhr.setRequestHeader('Authorization', header);
-         Gcontacts.xhr.withCredentials = true;
-         Gcontacts.xhr.onload = (callback != undefined) ? callback : Gcontacts.contacts_response;
-         Gcontacts.xhr.send();
+    };
+    get_group = function(group_link,callback){
+       if(token_data.valid){
+         contacts = config.contacts;
+         url = [contacts.by_group, 'group=', group_link, '&alt=',contacts.alt].join('');
+         header = [token_data.token_type,token_data.access_token].join(' ');
+         //get_xhr();
+         xhr.open('GET', url, true);
+         xhr.setRequestHeader('Authorization', header);
+         xhr.withCredentials = true;
+         xhr.onload = (callback != undefined) ? callback : get_contacts_response;
+         xhr.send();
        }else throw 'invalid token';
-    },
-    get_groups_response: function(){
-      Gcontacts.groups = JSON.parse(Gcontacts.xhr.response);
-      var groups = [];
-      with(Gcontacts.groups.feed){
-        for(i in entry)
-          groups.push(Object.create({},{ name: {value: entry[i].title.$t}, id: {value: entry[i].id.$t}}));
-      }
-      Gcontacts.events.groups.data = groups;
-      document.dispatchEvent(Gcontacts.events.groups);
-    },
-    get_contacts_response: function(){
-      Gcontacts.contacts = JSON.parse(Gcontacts.xhr.response);
-      var contacts = [];
-      with(Gcontacts.contacts.feed){
-        for(i in entry)
-          contacts.push(Object.create({},{ name: {value: entry[i].title.$t}, email: {value: entry[i].gd$email}}));
-      }
-      Gcontacts.events.contacts.data = contacts;
-      document.dispatchEvent(Gcontacts.events.contacts);
-    },
-    show_groups: function(){
-     if(Gcontacts.token_data.valid){
-      if(Gcontacts.groups.feed == undefined)
-        Gcontacts.get_groups(Gcontacts.get_groups_response)
-      else
-        with(Gcontacts.groups.feed)for(i in entry)console.log([entry[i].title.$t,entry[i].id.$t]);
+    };
+    get_groups_response =  function(){
+      groups = JSON.parse(xhr.response);
+      var _groups = [];
+        for(var i = 0, group; group = groups.feed.entry[i]; i++)
+          _groups.push(Object.create({},{ name: {value: group.title.$t}, id: {value: group.id.$t}}));
+      events.groups.data = _groups;
+      document.dispatchEvent(events.groups);
+    };
+    get_contacts_response = function(){
+      contacts = JSON.parse(xhr.response);
+      _contacts = [];
+        for(var i = 0,contact; contact = contacts.feed.entry[i]; i++)
+          _contacts.push(Object.create({},{ name: {value: contact.title.$t}, email: {value: contact.gd$email}}));
+      events.contacts.data = _contacts;
+      document.dispatchEvent(events.contacts);
+    };
+    show_groups = function(){
+     if(token_data.valid){
+      if(groups.feed == undefined) get_groups(get_groups_response)
+      else for(var i=0,group; group = groups.feed.entry[i]; i++) console.log([group.title.$t,group.id.$t]);
       }else throw 'get a valid token!';
-    },
-    get_contacts_by_group: function(group_name){
+    };
+    get_id_group = function(group_name){
+      for(var i=0,group;group = groups.feed.entry[i];i++)
+        if(group.title.$t == group_name) return group.id.$t
+    };
+    get_contacts_by_group = function(group_name){
       if((/^http(s?):\/\//).test(group_name))
-          Gcontacts.get_group(group_name,Gcontacts.get_contacts_response);
+          get_group(group_name,get_contacts_response);
       else{
-        if(Gcontacts.groups.feed != undefined){
-          var group = '';
-          with(Gcontacts.groups.feed)for(i in entry)if(entry[i].title.$t == group_name) group = entry[i].id.$t;
+        if(groups.feed != undefined){
+          group = get_id_group(group_name);
           if(group != '')
-            Gcontacts.get_group(group,Gcontacts.get_contacts_response)
+            get_group(group,get_contacts_response)
           else throw ['group',group_name,'not found'].join(' ');
         }else throw 'need get the contacts groups first with get_groups();';
       }
-    }
-};
+    };
+    return{
+      parameters: parameters,
+      init: init,
+      login: login,
+      groups: show_groups,
+      contacts_by_group: get_contacts_by_group
+    };
+}());
