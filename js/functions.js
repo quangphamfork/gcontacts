3c1114d653b831cee9ecc0c3b9fbefdc1ed2c63a
gagoar <xeroice@gmail.com>
2012-12-11 15:13:01 -0300

    better indent

--- js/functions.js
+++ js/functions.js
@@ -1,111 +1,112 @@
-var trade_of = function(select){
+var trade_of = function(select) {
   $('.cd-dropdown').replaceWith(select);
   $(dropdown).dropdown(random_dropdownEffect());
-  setTimeout(function(){$('.cd-dropdown span:first').trigger('mousedown.dropdown')},250);
+  setTimeout(function() {
+    $('.cd-dropdown span:first').trigger('mousedown.dropdown')
+  }, 250);
   $('ul').off('opened.click.dropdown');
   spinner.stop();
 };
-var retrive_from =  function(e){
+var go_back_from_groups = function(e) {
   spinner.spin($('.cd-dropdown span:first')[0]);
-  ( e.value ==  'from_contacts' ) ?  Gcontacts.groups() : Gcontacts.contacts_by_group(e.value);
+  (e.value == 'from_contacts') ? Gcontacts.groups() : Gcontacts.contacts_by_group(e.value);
 };
-var retrive_groups = function(e){
+var retrive_groups = function(e) {
   delete window._reference
   $('.cd-dropdown span:first').off('click');
   spinner.spin($('.cd-dropdown span:first')[0]);
   Gcontacts.groups();
 };
-var go_back_from_contacts = function(e){
+var go_back_from_contacts = function(e) {
   $('.cd-dropdown span:first').text(window._reference.title);
   spinner.spin($('.cd-dropdown span:first')[0]);
   Gcontacts.contacts_by_group(window._reference.id);
-
 }
-var retrive_contact_info = function(e){
-  switch(e.value){
-
-    case ( /^http:/ ).test(e.value) :
-      spinner.spin($('.cd-dropdown span:first')[0]);
+var retrive_contact_info = function(e) {
+  switch (e.value) {
+  case (/^http:/).test(e.value):
+    spinner.spin($('.cd-dropdown span:first')[0]);
     $('.cd-dropdown span:first').text(window._reference.title);
-      Gcontacts.contact_by_group(e.value);
-      break;
-    case 'from_contacts':
-      spinner.spin($('.cd-dropdown span:first')[0]);
-      Gcontacts.groups();
-      break;
-    default:
-      var addresses = e.value.split(' ');
-      var select = create_element();
-      var title = $(e.target).find('span:first').text();
-      select.options.add(new Option(title, -1));
-      if( addresses.length < 2 && addresses[0] == 'empty' )
-        select.options.add(new Option('no info about him/her yet', -2));
-      else
-        for(var i = 0,content; content = addresses[i];i++)
-      select.options.add(new Option(content, i))
+    Gcontacts.contact_by_group(e.value);
+    break;
+  case 'from_contacts':
+    spinner.spin($('.cd-dropdown span:first')[0]);
+    Gcontacts.groups();
+    break;
+  default:
+    var addresses = e.value.split(' ');
+    var select = create_element();
+    var title = $(e.target).find('span:first').text();
+    select.options.add(new Option(title, - 1));
+    if (addresses.length < 2 && addresses[0] == 'empty') select.options.add(new Option('no info about him/her yet', - 2));
+    else for (var i = 0, content; content = addresses[i]; i++)
+    select.options.add(new Option(content, i))
 
-      select.options.add(new Option('go back!', window._reference.id));
-      trade_of(select);
-      $('ul').on('opened.click.dropdown', go_back_from_contacts);
-      break;
+    select.options.add(new Option('go back!', window._reference.id));
+    trade_of(select);
+    $('ul').on('opened.click.dropdown', go_back_from_contacts);
+    break;
   }
 };
-var replace_contacts = function(e){
+var replace_contacts = function(e) {
   var title = window._reference && window._reference.title || $('.cd-dropdown span:first').text();
   var data = e.data;
   var select = create_element();
-  select.options.add(new Option(title, -1));
-  window._reference ={id: e.reference, title: title}
-  for(var i = 0,label; label = data[i];i++)
-    {
+  select.options.add(new Option(title, - 1));
+  window._reference = {
+      id: e.reference,
+      title: title
+  }
+  for (var i = 0, label; label = data[i]; i++) {
       var emails = [];
-      if( typeof( label.email ) !== 'string' )
-        for(var j = 0, email,name; email = label.email[j]; j++)
-          emails.push(email.address);
-          name = label.name.length < 20 ? label.name : [label.name.slice(0,20),'...'].join('');
-      select.options.add(new Option(name, ( emails.length > 0 ) ? emails.join(' ') : 'empty'));
-    }
-  select.options.add(new Option('back','from_contacts'))
-  trade_of(select);
-  $('ul').on('opened.click.dropdown', retrive_contact_info);
+      if (typeof(label.email) !== 'string') for (var j = 0, email, name; email = label.email[j]; j++)
+      emails.push(email.address);
+      name = label.name.length < 20 ? label.name : [label.name.slice(0, 20), '...'].join('');
+      select.options.add(new Option(name, (emails.length > 0) ? emails.join(' ') : 'empty'));
+  }
+   select.options.add(new Option('back', 'from_contacts'))
+   trade_of(select);
+   $('ul').on('opened.click.dropdown', retrive_contact_info);
 };
-var replace_groups = function(e){
+var replace_groups = function(e) {
   delete window._reference
   var data = e.data;
   var select = create_element();
-  var title = ( e.title.length < 20 ) ? e.title : [ e.title.slice(0,20),'...'].join('');
-  select.options.add(new Option(title, -1));
-  for(var i = 0,label,name; label = data[i];i++){
-    name = ( label.name.length < 20 ) ? label.name : [label.name.slice(0,20),'...'].join('');
-    select.options.add(new Option(label.name,label.id));
+  var title = (e.title.length < 20) ? e.title : [e.title.slice(0, 20), '...'].join('');
+  select.options.add(new Option(title, - 1));
+  for (var i = 0, label, name; label = data[i]; i++) {
+      name = (label.name.length < 20) ? label.name : [label.name.slice(0, 20), '...'].join('');
+      select.options.add(new Option(label.name, label.id));
   }
 
   trade_of(select);
-  $('ul').on('opened.click.dropdown', retrive_from);
+  $('ul').on('opened.click.dropdown', go_back_from_groups);
 };
-var create_element = function(){
+var create_element = function() {
   var select = document.createElement('select');
   select.id = 'dropdown';
   return select;
 };
-var no_groups_found = function(e){
+var no_groups_found = function(e) {
   var select = create_element();
-  select.options.add(new Option(e.title, -1));
-  select.options.add(new Option('no groups, try again later!', '2' ));
+  select.options.add(new Option(e.title, - 1));
+  select.options.add(new Option('no groups, try again later!', '2'));
   trade_of(select);
   $('ul').on('opened.click.dropdown', retrive_groups);
-};
-var no_contacts_found = function(e){
+    };
+var no_contacts_found = function(e) {
   var select = create_element();
   var title = $('.cd-dropdown span:first').text();
-  select.options.add(new Option(title, -1));
+  select.options.add(new Option(title, - 1));
   select.options.add(new Option('no contacts here!', '1'));
-  select.options.add(new Option('go back!', '2' ));
+  select.options.add(new Option('go back!', '2'));
   trade_of(select);
   $('ul').on('opened.click.dropdown', retrive_groups);
-};
-var random_dropdownEffect = function(){
-  var index = Math.floor(Math.random() * dropEffects.length);
+    };
+var random_dropdownEffect = function() {
+  var index = Math.floor( Math.random() * dropEffects.length );
   var picked = dropEffects[index];
   return picked
 }
+
+
